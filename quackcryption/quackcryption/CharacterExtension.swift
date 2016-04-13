f5bbc8d73bb840120ecc0ea808508f2f000e184b
Cole Johnson <john1739@purdue.edu>
2016-04-12 21:29:56 -0400

    Initial Commit

---
+++ quackcryption/quackcryption/CharacterExtension.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterExtension.swift
+//  quackcryption
+//
+//  Created by Cole Johnson on 4/12/16.
+//  Copyright © 2016 Cole Johnson. All rights reserved.
+//
+
+extension Character {
+    func utf8Value() -> UInt8 {
+        for s in String(self).utf8 {
+            return s
+        }
+        return 0
+    }
+    
+    func utf16Value() -> UInt16 {
+        for s in String(self).utf16 {
+            return s
+        }
+        return 0
+    }
+    
+    func unicodeValue() -> UInt32 {
+        for s in String(self).unicodeScalars {
+            return s.value
+        }
+        return 0
+    }
+}
